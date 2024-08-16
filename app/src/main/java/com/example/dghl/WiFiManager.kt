c370c6277ef2eedfb541b70d32ce5cd45b511410
datsiii <dasha.tsivina@gmail.com>
2024-08-16 21:27:58 +0300

    One instance every time

--- app/src/main/java/com/example/dghl/WiFiManager.kt
+++ app/src/main/java/com/example/dghl/WiFiManager.kt
@@ -2,7 +2,9 @@ package com.example.dghl
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class WiFiManager @Inject constructor(private val settings: WiFiSettings) {
     fun connect() {
         settings.openConnection()
